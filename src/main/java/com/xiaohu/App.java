f44359c3f868407cd137bfa4c613c5923ff4a4ec
MaltAroma <102124427+MaltAroma@users.noreply.github.com>
2022-03-22 21:13:51 +0800

    Update App.java

--- src/main/java/com/xiaohu/App.java
+++ src/main/java/com/xiaohu/App.java
@@ -9,5 +9,6 @@ public class App {
         System.out.println("Hello Gitee!");
         System.out.println("Hello Push!");
         System.out.println("Hello SSH Push!");
+        System.out.println("Hello Pull!");
     }
 }
