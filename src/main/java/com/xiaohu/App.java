bb531ccb83a1c72546e9797fb35c7bc5a99adee7
HJH <2464955155@qq.com>
2022-03-22 20:42:23 +0800

    Idea Third Commit

--- src/main/java/com/xiaohu/App.java
+++ src/main/java/com/xiaohu/App.java
@@ -5,6 +5,6 @@ public class App {
         System.out.println("Hello Git!");
         System.out.println("Hello Java!");
         System.out.println("Hello World");
-        System.out.println("Hello Idea!");
+        System.out.println("Hello GitHub!");
     }
 }
