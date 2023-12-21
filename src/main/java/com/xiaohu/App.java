fcfc47f02191335ce4ed13860af13ba0d09b792a
MaltAroma <2464955155@qq.com>
2023-12-21 11:51:20 +0800

    a git study demo commit

--- src/main/java/com/xiaohu/App.java
+++ src/main/java/com/xiaohu/App.java
@@ -10,5 +10,6 @@ public class App {
         System.out.println("Hello Push!");
         System.out.println("Hello SSH Push!");
         System.out.println("Hello Pull!");
+        System.out.println("Hello GitStudyDemo!");
     }
 }
